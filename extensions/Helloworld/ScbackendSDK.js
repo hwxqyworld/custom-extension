03c723fc8a87a9f65c11d4bf3b01445f475ee0ef
XQYWorld <xqyworld_gh@wpgzs.top>
2025-11-04 13:16:17 +0800

    修改Scbackend

--- extensions/Helloworld/ScbackendSDK.js
+++ extensions/Helloworld/ScbackendSDK.js
@@ -15,7 +15,8 @@
       "scbackendsdk.isconnected": "\u5DF2\u8FDE\u63A5\uFF1F",
       "scbackendsdk.send": "\u53D1\u9001\u6D88\u606F [msg]",
       "scbackendsdk.whenmessage": "\u5F53\u6536\u5230\u6D88\u606F",
-      "scbackendsdk.getmsg": "\u6536\u5230\u7684\u6D88\u606F"
+      "scbackendsdk.getmsg": "\u6536\u5230\u7684\u6D88\u606F",
+      "scbackendsdk.ping": "\u5EF6\u8FDF(\u6BEB\u79D2)"
     },
     en: {
       "scbackendsdk.connect": "Connect to [remaddr] server [instname] instance",
@@ -24,7 +25,8 @@
       "scbackendsdk.isconnected": "Is connected?",
       "scbackendsdk.send": "Send message [msg]",
       "scbackendsdk.whenmessage": "When message received",
-      "scbackendsdk.getmsg": "Received message"
+      "scbackendsdk.getmsg": "Received message",
+      "scbackendsdk.ping": "Ping (ms)"
     }
   };
 
@@ -45,6 +47,7 @@
       constructor() {
         this.ws = null;
         this.isopened = false;
+        this.delay = -1;
       }
       getInfo() {
         return {
@@ -92,6 +95,11 @@
               text: translate("scbackendsdk.isconnected")
             },
             {
+              blockType: Scratch2.BlockType.REPORTER,
+              opcode: "ping",
+              text: translate("scbackendsdk.ping")
+            },
+            {
               blockType: Scratch2.BlockType.COMMAND,
               opcode: "send",
               text: translate("scbackendsdk.send"),
@@ -113,42 +121,82 @@
               opcode: "getmsg",
               text: translate("scbackendsdk.getmsg")
             }
-          ],
-          menus: {
-            blockMenu: {
-              acceptReporters: true,
-              items: [{ text: translate("scbackendsdk.connect.wait"), value: true }, { text: translate("scbackendsdk.connect.dontwait"), value: false }]
-            }
-          }
+          ]
         };
       }
       _doconnect(remaddr, dst, util, resolve, reject) {
-        this.ws = new WebSocket(remaddr);
-        this.ws.onopen = () => {
-          this.ws.send(JSON.stringify({ type: "handshake", dst: dst.toString() }));
+        let attempts = 0;
+        const maxAttempts = 5;
+        const connectWS = () => {
+          if (this.isopened) return;
+          this.ws = new WebSocket(remaddr);
+          let interval;
+          this.ws.onopen = () => {
+            this.ws.send(JSON.stringify({ type: "handshake", dst: dst.toString() }));
+          };
+          this.ws.onmessage = (event) => {
+            const msg = event.data;
+            const data = JSON.parse(msg);
+            switch (data.type) {
+              case "handshake":
+                if (data.status === "ok") {
+                  this.isopened = true;
+                  this.sessionid = data.sessionId;
+                  interval = setInterval(() => {
+                    if (this.ws && this.isopened) {
+                      this.dtimer = Date.now();
+                      this.ws.send(JSON.stringify({ type: "ping" }));
+                    }
+                  }, 5e3);
+                  if (resolve) resolve();
+                } else {
+                  this.ws.close();
+                  this.ws = null;
+                  this.isopened = false;
+                  attempts++;
+                  if (attempts < maxAttempts) {
+                    setTimeout(connectWS, 1e3);
+                  } else {
+                    util.startHats("scbackendsdk_whenerror");
+                    if (reject) reject(new Error("Handshake failed"));
+                  }
+                }
+                break;
+              case "message":
+                util.startHats("scbackendsdk_whenmessage").forEach((t) => {
+                  t.initParams();
+                  t.pushParam("data", data.message);
+                });
+                break;
+              case "pong":
+                this.delay = Date.now() - this.dtimer;
+                break;
+            }
+          };
+          this.ws.onclose = (event) => {
+            let isopened = this.isopened;
+            this.ws = null;
+            this.isopened = false;
+            if (isopened) return;
+            attempts++;
+            if (attempts < maxAttempts) {
+              setTimeout(connectWS, 1e3);
+            } else {
+              util.startHats("scbackendsdk_whenerror");
+              if (reject) reject(event);
+            }
+          };
+          this.ws.onerror = (error) => {
+            attempts++;
+            if (attempts < maxAttempts) {
+              setTimeout(connectWS, 1e3);
+            } else {
+              util.startHats("scbackendsdk_whenerror");
+              if (reject) reject(error);
+            }
+          };
         };
-        this.ws.onmessage = (event) => {
-          const msg = event.data;
-          const data = JSON.parse(msg);
-          if (data.type === "handshake" && data.status === "ok") {
-            this.isopened = true;
-            this.sessionid = data.sessionId;
-            if (resolve) resolve();
-          } else if (data.type === "message") {
-            util.startHats("scbackendsdk_whenmessage").forEach((t) => {
-              t.initParams();
-              t.pushParam("data", data.message);
-            });
-          }
-        };
-        this.ws.onclose = (event) => {
-          this.ws = null;
-          this.isopened = false;
-          if (reject) reject(event);
-        };
-        this.ws.onerror = (error) => {
-          if (reject) reject(error);
-        };
+        connectWS();
       }
       connect(args, util) {
         if (this.ws) {
@@ -158,8 +206,8 @@
         }
         this._doconnect(args.remaddr, args.instname, util);
       }
-      async connectwait(args, util) {
-        await new Promise((resolve, reject) => {
+      connectwait(args, util) {
+        return new Promise((resolve, reject) => {
           this._doconnect(args.remaddr, args.instname, util, resolve, reject);
         });
       }
@@ -167,11 +215,13 @@
         if (this.ws) {
           this.ws.close();
           this.ws = null;
-          this.isopened = false;
         }
       }
       isconnected() {
         return this.isopened;
+      }
+      ping() {
+        return this.delay;
       }
       send(args) {
         if (this.ws && this.isopened) {
